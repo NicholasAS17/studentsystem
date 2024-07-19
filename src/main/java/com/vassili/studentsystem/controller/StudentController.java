639091b64eab45bf355efc34f2bee8f6949a9300
Nicholas <simpsonnicholas220@icloud.com>
2024-07-19 03:54:45 -0400

    Added Postman configurations

--- src/main/java/com/vassili/studentsystem/controller/StudentController.java
+++ src/main/java/com/vassili/studentsystem/controller/StudentController.java
@@ -1,7 +1,24 @@
 package com.vassili.studentsystem.controller;
 
-public class StudentController {
+import com.vassili.studentsystem.model.Student;
+import com.vassili.studentsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student) {
+        studentService.saveStudent(student);
+        return "New student added";
+    }
 
 
 }
--- src/main/java/com/vassili/studentsystem/service/StudentService.java
+++ src/main/java/com/vassili/studentsystem/service/StudentService.java
@@ -1,7 +1,7 @@
 package com.vassili.studentsystem.service;
 
+import com.vassili.studentsystem.model.Student;
+
 public interface StudentService {
-
-
-
+    public Student saveStudent(Student student);
 }
